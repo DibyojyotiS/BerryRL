3aa0a1d27919ac944263aad5fe898f85fa19e44c
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-02-09 03:01:45 +0530

    upload

--- new_script copy.py
+++ new_script copy.py
@@ -5,7 +5,7 @@ import torch
 from torch import Tensor, nn
 import torch.nn.functional as F
 from torch.optim.rmsprop import RMSprop
-from DRLagents import VPG, softMaxAction
+from DRLagents import DDQN, epsilonGreedyAction, greedyAction, PrioritizedExperienceRelpayBuffer
 
 TORCH_DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -54,18 +54,24 @@ def make_net(inDim, outDim, hDim, output_probs=False):
     class net(nn.Module):
         def __init__(self, inDim, outDim, hDim, activation = F.relu):
             super(net, self).__init__()
+            self.outDim = outDim
             self.inputlayer = nn.Linear(inDim, hDim[0])
             self.hiddenlayers = nn.ModuleList([nn.Linear(hDim[i], hDim[i+1]) for i in range(len(hDim)-1)])
             self.outputlayer = nn.Linear(hDim[-1], outDim)
             self.activation = activation
+            if outDim > 1 and not output_probs:
+                self.actadvs = nn.Linear(hDim[-1], outDim)
 
         def forward(self, x: torch.Tensor) -> torch.Tensor:
             t = self.activation(self.inputlayer(x))
             for layer in self.hiddenlayers:
                 t = self.activation(layer(t))
-            t = self.outputlayer(t)
-            if output_probs: t = F.log_softmax(t, -1)
-            return t
+            o = self.outputlayer(t) # value or probs
+            if output_probs: o = F.log_softmax(o, -1)
+            elif self.outDim > 1:
+                advs = self.actadvs(t)
+                o = o + (advs - advs.mean())
+            return o
     
     netw = net(inDim, outDim, hDim)
     netw.to(TORCH_DEVICE)
@@ -77,17 +83,15 @@ def make_net(inDim, outDim, hDim, output_probs=False):
 berry_env = BerryFieldEnv_MatInput(no_action_r_threshold=0.6)
 
 # init models
-valuemodel = make_net(3*8, 1, [16,8])
-policymodel = make_net(3*8, 9, [16,8], output_probs=True)
+value_net = make_net(3*8, 9, [16,8])
+buffer = PrioritizedExperienceRelpayBuffer(int(1E6), 0.9, 0.2, 0.001)
 
 # init optimizers
-voptim = RMSprop(valuemodel.parameters(), lr=0.01)
-poptim = RMSprop(policymodel.parameters(), lr=0.01)
-tstrat = softMaxAction(policymodel, outputs_LogProbs=True)
+optim = RMSprop(value_net.parameters(), lr=0.01)
+tstrat = epsilonGreedyAction(value_net, 0.5, 0.01, 50)
+estrat = greedyAction(value_net)
 
-agent = VPG(berry_env, policymodel, valuemodel, tstrat, poptim, voptim, make_state, gamma=0.99,
-                MaxTrainEpisodes=500, MaxStepsPerEpisode=12000, beta=0.1, value_steps=100,
-                trajectory_seg_length=2000, skipSteps=20, printFreq=1, device= TORCH_DEVICE)
-
+agent = DDQN(berry_env, value_net, tstrat, optim, buffer, 512, gamma=0.99, 
+                skipSteps=50, make_state=make_state, printFreq=1, device=TORCH_DEVICE)
 trianHist = agent.trainAgent(render=True)
-evalHist = agent.evaluate(tstrat, 10, True)
+evalHist = agent.evaluate(estrat, 10, True)
