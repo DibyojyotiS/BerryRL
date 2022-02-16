5e2abdb7afe4e52e88ed2557129c08674d1d45e4
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-02-16 15:05:59 +0530

    updates

--- new_script_PERD3QN.py
+++ new_script_PERD3QN.py
@@ -65,11 +65,12 @@ if __name__ == "__main__":
         episode_count = -1
         def reset(**args):
             nonlocal t, n, p, r, episode_count
-            
+
             print('episode:',episode_count, 
                 'berries picked:', berry_env.get_numBerriesPicked(),
-                'of', n*p, 'patches:', p)
+                'of', n*p, 'patches:', p, end='')
             if episode_count>=0:print('positive-in-buffer:',sum(buffer.buffer['reward'].cpu()>0).item())
+            else: print()
 
             # random berries
             patch_centroids = np.reshape(np.random.randint(400, 3600, size=2*p), (p,2))
