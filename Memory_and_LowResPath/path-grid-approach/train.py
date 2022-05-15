a1eb0c30bfa2d88d70d23d8c4f0acbfb8974b257
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-05-15 15:14:22 +0530

    changed optimizer from RMSprop TO Adam and hyperparameter changes

--- Memory_and_LowResPath/path-grid-approach/train.py
+++ Memory_and_LowResPath/path-grid-approach/train.py
@@ -1,6 +1,7 @@
 import time
 import shutil
 from DRLagents import *
+from torch.optim.adam import Adam
 from torch.optim.rmsprop import RMSprop
 from get_baby_env import getBabyEnv
 from make_net import *
@@ -9,8 +10,8 @@ from Agent import *
 # baby env params
 FIELD_SIZE = (5000,5000)
 PATCH_SIZE = (1000,1000)
-N_PATCHES = 6
-N_BERRIES = 20
+N_PATCHES = 5
+N_BERRIES = 50
 
 LOG_DIR = os.path.join('.temp' , '{}-{}-{} {}-{}-{}'.format(*time.gmtime()[0:6]))
 TORCH_DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -19,21 +20,19 @@ if __name__ == '__main__':
 
     print(LOG_DIR)
 
-    # copy all files into log-dir
+    # copy all files into log-dir and setup logging
     dest = os.path.join(LOG_DIR, 'pyfiles-backup')
     if not os.path.exists(dest): os.makedirs(dest)
     for file in [f for f in os.listdir('.') if f.endswith('.py')]: shutil.copy2(file, dest)
-
-    # setup logging
     logger = StdoutLogger(filename=os.path.join(LOG_DIR, 'log.txt'))
 
-    berry_env = getBabyEnv(FIELD_SIZE, PATCH_SIZE, N_PATCHES, N_BERRIES, LOG_DIR, living_cost=True)
+    # setup env and model
+    berry_env = getBabyEnv(FIELD_SIZE, PATCH_SIZE, N_PATCHES, N_BERRIES, LOG_DIR, living_cost=True, initial_juice=0.1)
     agent = Agent(berry_env)
-
     nnet = agent.getNet(TORCH_DEVICE); print(nnet)
-
+    
     buffer = PrioritizedExperienceRelpayBuffer(int(2E4), alpha=0.8, beta=0.1, beta_rate=0.01)
-    optim = RMSprop(nnet.parameters(), lr=0.001)
+    optim = Adam(nnet.parameters(), lr=0.0001)
     tstrat = epsilonGreedyAction(nnet, 0.5, 0.01, 400)
     estrat = greedyAction(nnet)
 
@@ -44,12 +43,13 @@ if __name__ == '__main__':
             print('-> berries picked:', berry_env.get_numBerriesPicked(),
                 'of', berry_env.get_totalBerries(), '| patches-visited:', visited_patches, 
                 '| positive-in-buffer:', sum(buffer.buffer['reward'].cpu()>0).item(),
-                f'| amount-filled: {100*len(buffer)/buffer.bufferSize:.2f}')
+                f'| amount-filled: {100*len(buffer)/buffer.bufferSize:.2f}%')
+            print(f'\t| approx positives in sample {256}: {sum(buffer.sample(256)[0]["reward"].cpu()>0).item()}')
 
     ddqn_trainer = DDQN(berry_env, nnet, tstrat, optim, buffer, batchSize=256, skipSteps=10,
                         make_state=agent.makeState, make_transitions=agent.makeStateTransitions,
-                        gamma=0.9, MaxTrainEpisodes=400, user_printFn=print_fn,
-                        printFreq=1, update_freq=1, polyak_tau=0.2, polyak_average= True,
+                        gamma=0.99, MaxTrainEpisodes=400, user_printFn=print_fn,
+                        printFreq=1, update_freq=1, polyak_tau=0.5, polyak_average= True,
                         log_dir=LOG_DIR, save_snapshots=True, device=TORCH_DEVICE)
     trianHist = ddqn_trainer.trainAgent(render=False)
     ddqn_trainer.evaluate(estrat, render=True)
