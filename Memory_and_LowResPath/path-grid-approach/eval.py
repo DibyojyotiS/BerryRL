02421e4cc45ffbe6eccbbc01ee9524ae762d8fc7
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-05-16 02:38:43 +0530

    removed some code

--- Memory_and_LowResPath/path-grid-approach/eval.py
+++ Memory_and_LowResPath/path-grid-approach/eval.py
@@ -22,26 +22,16 @@ if __name__ == '__main__':
 
     nnet = agent.getNet(TORCH_DEVICE)
 
-    nnet.load_state_dict(torch.load('.temp\\2022-5-15 14-51-33\\trainLogs\\onlinemodel_weights_episode_170.pth'))
+    nnet.load_state_dict(torch.load('.temp\\2022-5-15 21-0-59\\trainLogs\\onlinemodel_weights_episode_3.pth'))
     nnet.eval()
 
-    buffer = PrioritizedExperienceRelpayBuffer(int(1E5), 0.95, 0.1, 0.01)
-    optim = RMSprop(nnet.parameters(), lr=0.0001)
-    tstrat = epsilonGreedyAction(nnet, 0.5, 0.01, 50)
+    buffer = None; optim = None; tstrat = None
     estrat = softMaxAction(nnet,temperature=1)
 
-    # an user-print-function to print extra stats
-    def print_fn():
-        if buffer.buffer is not None:
-            visited_patches = [p for p in berry_env.patch_visited.keys() if berry_env.patch_visited[p] > 0]
-            print('-> berries picked:', berry_env.get_numBerriesPicked(),
-                'of', berry_env.get_totalBerries(), '| patches-visited:', visited_patches, 
-                '| positive-in-buffer:', sum(buffer.buffer['reward'].cpu()>0).item())
-
-    ddqn_trainer = DDQN(berry_env, nnet, tstrat, optim, buffer, batchSize=128, skipSteps=10,
+    ddqn_trainer = DDQN(berry_env, nnet, tstrat, optim, buffer, batchSize=256, skipSteps=10,
                         make_state=agent.makeState, make_transitions=agent.makeStateTransitions,
-                        gamma=0.9, MaxTrainEpisodes=50, user_printFn=print_fn,
-                        printFreq=1, update_freq=2, polyak_tau=0.8, polyak_average= True,
+                        gamma=0.9, MaxTrainEpisodes=50, user_printFn=None,
+                        printFreq=1, update_freq=2, polyak_tau=0.2, polyak_average= True,
                         log_dir=LOG_DIR, save_snapshots=True, device=TORCH_DEVICE)
     ddqn_trainer.evaluate(estrat, render=True)
 
