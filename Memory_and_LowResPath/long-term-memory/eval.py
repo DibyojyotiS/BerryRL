77586beb49bbda1b940f9eff85b0cef270f30845
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-05-27 09:51:58 +0530

    modified

--- Memory_and_LowResPath/long-term-memory/eval.py
+++ Memory_and_LowResPath/long-term-memory/eval.py
@@ -14,23 +14,22 @@ TORCH_DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 if __name__ == '__main__':
 
-    berry_env = getBabyEnv(FIELD_SIZE, PATCH_SIZE, N_PATCHES, N_BERRIES, LOG_DIR, #initial_juice=0.1,
-                            end_on_boundary_hit=False, allow_no_action=False, show=False)
-    # berry_env = BerryFieldEnv()
+    # berry_env = getBabyEnv(FIELD_SIZE, PATCH_SIZE, N_PATCHES, N_BERRIES, LOG_DIR, #initial_juice=0.1,
+                            # end_on_boundary_hit=False, allow_no_action=False, show=False)
+    berry_env = BerryFieldEnv()
     agent = Agent(berry_env, mode='eval', debug=True, noise=0.025, persistence=0.7)
 
     nnet = agent.getNet(TORCH_DEVICE)
 
-    nnet.load_state_dict(torch.load('..\\trainLogs\\onlinemodel_weights_episode_263.pth'))
+    nnet.load_state_dict(torch.load('..\\trainLogs\models\episode-171\\onlinemodel_statedict.pt'))
     nnet.eval()
 
     buffer = None; optim = None; tstrat = None
     estrat = greedyAction(nnet)
 
     ddqn_trainer = DDQN(berry_env, nnet, tstrat, optim, buffer, batchSize=256, skipSteps=10,
-                        make_state=agent.makeState, make_transitions=agent.makeStateTransitions,
-                        gamma=0.9, MaxTrainEpisodes=50, user_printFn=None,
-                        printFreq=1, log_dir=LOG_DIR, save_snapshots=True, device=TORCH_DEVICE)
+                        make_state=agent.makeState, log_dir=LOG_DIR, device=TORCH_DEVICE)
     ddqn_trainer.evaluate(estrat, render=True)
+    print(berry_env.get_numBerriesPicked())
 
     agent.showDebug(nnet)
