77ab7b4de6eac912a25d34f11568126d32a1278f
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-05-28 18:43:00 +0530

    added tool to see agent path

--- Memory_and_LowResPath/long-term-memory/print_utils.py
+++ Memory_and_LowResPath/long-term-memory/print_utils.py
@@ -1,5 +1,10 @@
-import torch
+import pickle
+import matplotlib.pyplot as plt
 import numpy as np
+import torch
+from berry_field.envs.berry_field_env import BerryFieldEnv
+from matplotlib.patches import Rectangle
+
 
 def printLocals(name, locals:dict):
     print(name,':')
@@ -27,3 +32,23 @@ def my_print_fn(berry_env, buffer, tstrat, ssize=256):
             print(f'\t| approx positives in sample {ssize}: {positive_idxsmp.shape[-1]}')
             print(f'\t| approx action-dist in sample {ssize}: {cpunptolist(asmp)} {cpunptolist(countsmp)}')
     return print_fn
+
+def picture_episode(LOG_DIR, episode, K=10, figsize=(10,10)):
+    # open the berry_field pickle and draw patches and berries
+    path = f'{LOG_DIR}/analytics-berry-field/{episode}/berryenv.obj'
+    with open(path, 'rb') as f: berry_field:BerryFieldEnv = pickle.load(f)
+    berry_data = berry_field.berry_collision_tree.boxes
+    patch_data = berry_field.patch_tree.boxes
+    fig, ax = plt.subplots(figsize=figsize)
+    ax.scatter(x=berry_data[:,0],y=berry_data[:,1],s=berry_data[:,2],c='red')
+    ax.add_patch(Rectangle((0,0), *berry_field.FIELD_SIZE, fill=False))
+    for x,y,pw,ph in patch_data: 
+        ax.add_patch(Rectangle((x-pw/2,y-ph/2), pw, ph, fill=False))
+
+    # open the agent path and draw the path with K fold decimation
+    path = f'{LOG_DIR}/analytics-berry-field/{episode}/agent_path.txt'
+    with open(path, 'r') as f: agentpath = eval('['+f.readline()+']')
+    print('total steps', len(agentpath)-1)
+    agentpath = np.array(agentpath[::K])
+    ax.plot(agentpath[:,0],agentpath[:,1])
+    plt.show()
