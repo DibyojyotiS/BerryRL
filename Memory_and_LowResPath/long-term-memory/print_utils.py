202810d4a294908293db2ba84d2c5e7a9c52157c
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-05-30 12:25:08 +0530

    multiprocessing in gif making

--- Memory_and_LowResPath/long-term-memory/print_utils.py
+++ Memory_and_LowResPath/long-term-memory/print_utils.py
@@ -34,7 +34,8 @@ def my_print_fn(berry_env, buffer, tstrat, ssize=256):
             print(f'\t| approx action-dist in sample {ssize}: {cpunptolist(asmp)} {cpunptolist(countsmp)}')
     return print_fn
 
-def picture_episode(LOG_DIR, episode, K=10, figsize=(10,10), title=None, show=True):
+def picture_episode(LOG_DIR, episode, K=10, figsize=(10,10), title=None, show=True, 
+                    alpha=1, pathwidth=1, savepth=None, close=False):
     """ plt plot showing patches, berries and the agent path """
     # open the berry_field pickle and draw patches and berries
     path = f'{LOG_DIR}/analytics-berry-field/{episode}/berryenv.obj'
@@ -42,30 +43,41 @@ def picture_episode(LOG_DIR, episode, K=10, figsize=(10,10), title=None, show=Tr
     berry_data = berry_field.berry_collision_tree.boxes
     patch_data = berry_field.patch_tree.boxes
     fig, ax = plt.subplots(figsize=figsize)
-    ax.scatter(x=berry_data[:,0],y=berry_data[:,1],s=berry_data[:,2],c='red')
-    # for berry in berry_data:
-    #     ax.add_patch(CirclePolygon(berry[:2], berry[-1],color='r'))
+    ax.scatter(x=berry_data[:,0],y=berry_data[:,1],s=berry_data[:,2],c='red', alpha=alpha)
     ax.add_patch(Rectangle((0,0), *berry_field.FIELD_SIZE, fill=False))
     for x,y,pw,ph in patch_data: 
-        ax.add_patch(Rectangle((x-pw/2,y-ph/2), pw, ph, fill=False))
+        ax.add_patch(Rectangle((x-pw/2,y-ph/2), pw, ph, fill=False, alpha=alpha))
 
     # open the agent path and draw the path with K fold decimation
     path = f'{LOG_DIR}/analytics-berry-field/{episode}/agent_path.txt'
     with open(path, 'r') as f: agentpath = eval('['+f.readline()+']')
     print('total steps', len(agentpath)-1)
     agentpath = np.array(agentpath[::K])
-    ax.plot(agentpath[:,0],agentpath[:,1])
+    ax.plot(agentpath[:,0],agentpath[:,1],linewidth=pathwidth)
 
     if title: plt.title(title)
+    if savepth: plt.savefig(savepth)
     if show: plt.show()
+    if close: plt.close()
 
-def picture_episodes_gif(fname, LOG_DIR, episodes, K=10, figsize=(10,10), titlefmt='', duration=0.5):
+def picture_episodes_gif(fname, LOG_DIR, episodes, K=10, figsize=(10,10), titlefmt='', 
+                        alpha=1, pathwidth=1, duration=0.5, nparallel=0):
     if not fname.endswith('.gif'): fname+='.gif'
+    if not os.path.exists('.tmpgif'): os.makedirs('.tmpgif')
+
+    def argen(i):
+        return LOG_DIR,i,K,figsize,titlefmt.format(i),False,\
+            alpha,pathwidth,f'.tmpgif/temp_pic_episode_img_{i}.png',True
+
+    if nparallel:
+        import multiprocessing as mp
+        with mp.Pool(nparallel) as pool: 
+            pool.starmap(picture_episode, [argen(i) for i in episodes])
+
     with imageio.get_writer(fname, duration=duration) as f:
         for i in episodes:
-            picture_episode(LOG_DIR,i,K,figsize,titlefmt.format(i),False)
-            plt.savefig('temp_pic_episode_img.png')
-            plt.close()
-            img = imageio.imread('temp_pic_episode_img.png')
+            if not nparallel: picture_episode(*argen(i))
+            img = imageio.imread(f'.tmpgif/temp_pic_episode_img_{i}.png')
             f.append_data(img)
-            os.remove('temp_pic_episode_img.png')
+            os.remove(f'.tmpgif/temp_pic_episode_img_{i}.png')
+    os.removedirs('.tmpgif')
