5f1f251da3da16d9e3efffdcab9f582a777dee77
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-06-01 15:49:20 +0530

    restructure functions

--- Memory_and_LowResPath/long-term-memory/print_utils.py
+++ Memory_and_LowResPath/long-term-memory/print_utils.py
@@ -12,27 +12,32 @@ def printLocals(name, locals:dict):
     for k in locals: print('\t',k,':',locals[k])
     print('\n')
 
-def my_print_fn(berry_env, buffer, tstrat, ssize=256):
-    cpunptolist = lambda t: t.cpu().numpy().tolist()
+def Env_print_fn(berry_env:BerryFieldEnv):
+    visited_patches = [p for p in berry_env.patch_visited.keys() \
+                        if berry_env.patch_visited[p] > 0]
+    print('-> berries picked:', berry_env.get_numBerriesPicked(),
+        'of', berry_env.get_totalBerries(), '| patches-visited:', visited_patches,
+        f'| juice left:{berry_env.total_juice:.2f}')
+    
+def my_print_fn(berry_env:BerryFieldEnv, buffer, tstrat, ssize=256):
+    tolist = lambda t: t.cpu().numpy().tolist()
     def print_fn():
+        Env_print_fn(berry_env)
+        print('\t| epsilon:', tstrat.epsilon)
         if buffer.buffer is not None:
-            visited_patches = [p for p in berry_env.patch_visited.keys() if berry_env.patch_visited[p] > 0]
             positive_idx = np.argwhere((buffer.buffer['reward']>0).cpu().squeeze())
             a,count = torch.unique(buffer.buffer['action'][positive_idx], return_counts=True)
-
             sample = buffer.sample(ssize)[0]
             positive_idxsmp = np.argwhere((sample["reward"]>0).cpu().squeeze())
             asmp,countsmp = torch.unique(sample['action'][positive_idxsmp], return_counts=True)
-
-            print('-> berries picked:', berry_env.get_numBerriesPicked(),
-                'of', berry_env.get_totalBerries(), '| patches-visited:', visited_patches, 
-                '| positive-in-buffer:', positive_idx.shape[-1],
+            # print the stuffs!
+            print('\t| positive-in-buffer:', positive_idx.shape[-1],
                 f'| amount-filled: {100*len(buffer)/buffer.bufferSize:.2f}%')
-            print('\t| epsilon:', tstrat.epsilon)
-            print('\t| action-stats: ', cpunptolist(a), cpunptolist(count))
+            print('\t| action-stats: ', tolist(a), tolist(count))
             print(f'\t| approx positives in sample {ssize}: {positive_idxsmp.shape[-1]}')
-            print(f'\t| approx action-dist in sample {ssize}: {cpunptolist(asmp)} {cpunptolist(countsmp)}')
+            print(f'\t| approx action-dist in sample {ssize}: {tolist(asmp)} {tolist(countsmp)}')
     return print_fn
+
 
 def picture_episode(LOG_DIR, episode, K=10, figsize=(10,10), title=None, show=True, 
                     alpha=1, pathwidth=1, savepth=None, close=False):
