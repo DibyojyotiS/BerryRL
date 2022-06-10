8dbdf059940a7cd2f6ac0ed7f897a7a3d2d83536
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-06-10 16:51:32 +0530

    restructure code

--- Memory_and_LowResPath/long-term-memory/train.py
+++ Memory_and_LowResPath/long-term-memory/train.py
@@ -6,20 +6,32 @@ from torch.optim.adam import Adam
 from torch.optim.rmsprop import RMSprop
 
 from Agent import *
-from get_baby_env import getBabyEnv
+from get_baby_env import env_step_wrapper, getRandomEnv
 from make_net import *
-from print_utils import my_print_fn
-
-# baby env params
-FIELD_SIZE = (20000,20000)
-PATCH_SIZE = (2600,2600)
-N_PATCHES = 10
-SEPERATION= 2400
-N_BERRIES = 80
+from print_utils import Env_print_fn, my_print_fn
 
 LOG_DIR = os.path.join('.temp' , '{}-{}-{} {}-{}-{}'.format(*time.gmtime()[0:6]))
-RESUME_DIR= '.temp\\2022-5-26 20-15-58'
+# LOG_DIR = '.temp/2022-6-10 5-10-29'
+RESUME_DIR= LOG_DIR
 TORCH_DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def random_train_env():
+    print("training on randomly generated env")
+    def spawn_radius_schedule(episode):
+        x = (1 + episode//100) * 100
+        print('\t| spawn_radius: ', x)
+        return x
+    trainEnv = getRandomEnv(field_size=(20000,20000), 
+        patch_size=(2600,2600), num_patches=10, seperation=2400, 
+        nberries=80, logDir=LOG_DIR, spawn_radius=100, 
+        penalize_boundary_hit=False)
+    return trainEnv
+
+def original_train_env():
+    print("training on original (fixed) env")
+    trainEnv = BerryFieldEnv(analytics_folder=LOG_DIR, penalize_boundary_hit=True)
+    trainEnv.step = env_step_wrapper(trainEnv.step, trainEnv.REWARD_RATE)
+    return trainEnv
 
 if __name__ == '__main__':
 
@@ -30,37 +42,46 @@ if __name__ == '__main__':
     if not os.path.exists(dest): os.makedirs(dest)
     for file in [f for f in os.listdir('.') if f.endswith('.py')]: shutil.copy2(file, dest)
 
-    # setup env and model and training params
-    berry_env = getBabyEnv(FIELD_SIZE, PATCH_SIZE, N_PATCHES, SEPERATION, 
-                            N_BERRIES, LOG_DIR, living_cost=True)
+    # setup eval env
+    evalEnv = BerryFieldEnv(analytics_folder=f'{LOG_DIR}/eval')
+
+    # for training on randomly generated envs
+    trainEnv = random_train_env()
+    # trainEnv = original_train_env() # uncomment to train on the original env
     
     # make the agent and network
-    agent = Agent(berry_env)
+    agent = Agent(trainEnv, nstep_transition=[1,30,60])
     nnet = agent.getNet(TORCH_DEVICE); print(nnet)
 
     # training stuffs
-    optim = Adam(nnet.parameters(), lr=0.00001)
-    buffer = PrioritizedExperienceRelpayBuffer(int(5E4), alpha=0.95, beta=0.1, beta_rate=0.9/2000)
-    tstrat = epsilonGreedyAction(0.5, 0.1, 2000)
-    print_fn = my_print_fn(berry_env, buffer, tstrat, 512)
+    optim = Adam(nnet.parameters(), lr=0.00002)
+    buffer = PrioritizedExperienceRelpayBuffer(int(6E4), alpha=0.96,
+                                        beta=0.1, beta_rate=0.9/2000)
+    tstrat = epsilonGreedyAction(0.5)
 
-    print('lr used = 0.00001, num_gradient_steps= 500')
-    print("optimizing the online-model after every 2000 actions (skipSteps=10)")
-    print("batch size=512, gamma=0.99, alpha=0.95")
-    print("polyak_tau=0.1, update_freq=10")
-    ddqn_trainer = DDQN(berry_env, nnet, tstrat, optim, buffer, batchSize=512, skipSteps=10,
+    print('lr used = 0.00002, num_gradient_steps= 500')
+    print("optimizing the online-model after every 2000 actions")
+    print("batch size=512, gamma=0.8, alpha=0.96")
+    print("polyak_tau=0.1, update_freq=5")
+    ddqn_trainer = DDQN(trainEnv, nnet, tstrat, optim, buffer, batchSize=512, 
+                        gamma=0.8, update_freq=5, MaxTrainEpisodes=2000, skipSteps=10,
+                        optimize_every_kth_action=2000, num_gradient_steps=500,
                         make_state=agent.makeState, make_transitions=agent.makeStateTransitions,
-                        gamma=0.99, MaxTrainEpisodes=2000, optimize_every_kth_action=2000, printFreq=1,
-                        user_printFn=print_fn, polyak_tau=0.1, polyak_average= True, num_gradient_steps= 500,
-                        update_freq=10, log_dir=LOG_DIR, snapshot_episode=1, resumeable_snapshot=3, 
-                        device=TORCH_DEVICE)
+                        evalFreq=10, printFreq=1, polyak_average=True, polyak_tau=0.1,
+                        log_dir=LOG_DIR, resumeable_snapshot=10, device=TORCH_DEVICE)
     
     # try to resume training
-    if resume_run: ddqn_trainer.attempt_resume(RESUME_DIR)
+    if resume_run: ddqn_trainer.attempt_resume(RESUME_DIR)    
 
-    # train, save optimizer on keyborad interupt
-    try: trianHist = ddqn_trainer.trainAgent(render=False)
+    # make print-fuctions to print extra info
+    trainPrintFn = my_print_fn(trainEnv, buffer, tstrat, ddqn_trainer)
+    evalPrintFn = lambda : Env_print_fn(evalEnv)
+
+    # start training! :D
+    try: trianHist = ddqn_trainer.trainAgent(evalEnv=evalEnv,
+            train_printFn=trainPrintFn, eval_printFn=evalPrintFn)
     except KeyboardInterrupt as kb: pass
 
-    ddqn_trainer.evaluate(render=True)
+    # final evaluation with render
+    ddqn_trainer.evaluate(evalEnv=evalEnv, render=True)
     logger.close()
