21fff12a3dc4c9675970de4614f8b716f6c9dc0b
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-06-16 11:14:46 +0530

    now activate exploration subroutine only when there are no berries in scene

--- DDQN-way/utils/exploration_subroutines/random_exploration.py
+++ DDQN-way/utils/exploration_subroutines/random_exploration.py
@@ -53,7 +53,7 @@ def random_exploration(berryenv:BerryFieldEnv, discount=1.0, render=False, rende
             p_action = action
 
             if not done and render and steps%renderS==0: berryenv.render()
-            if not ((len(listberries) == 0) or current_patch is None): break
+            if not (len(listberries) == 0): break
             if done: break
         
         return steps, listberries, reward_, done, info
