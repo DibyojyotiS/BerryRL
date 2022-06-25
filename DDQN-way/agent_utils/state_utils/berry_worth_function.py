d69a2e41f64fb371dde181442705f103eb5ff6c9
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-06-25 08:55:06 +0530

    updated doc-string

--- DDQN-way/agent_utils/state_utils/berry_worth_function.py
+++ DDQN-way/agent_utils/state_utils/berry_worth_function.py
@@ -2,7 +2,10 @@ def berry_worth(sizes, distances,
             REWARD_RATE, DRAIN_RATE, HALFDIAGOBS, WORTH_OFFSET=0):
     """ the reward that can be gained by pursuing a berry of given size and distance
     we note that the distances are scaled to be in range 0 to 1 by dividing by half-diag
-    of observation space """
+    of observation space.
+    To be noted that berryEnv.step returns observation as a list
+    of berries as [x,y,berry-size]. Where x and y are scaled by 
+    dividing by the half-diagonal of the observation space. """
     # for computation of berry worth, can help to change 
     # the agent's preference of different sizes of berries. 
     rr, dr = REWARD_RATE, DRAIN_RATE
