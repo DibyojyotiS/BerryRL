6cf27c3216f9e90ab3e08dc88aadff8828692796
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-08-21 18:04:30 +0530

    made config file

---
+++ DDQN-way/config.py
@@ -0,0 +1,69 @@
+# this will be a summary of what ever constant argument is required
+CONFIG = {
+
+    "RND_TRAIN_ENV": dict(
+        field_size=(20000,20000), 
+        patch_size=(2600,2600), 
+        num_patches=10, seperation=2400, 
+        nberries=80, spawn_radius=100, 
+        patch_with_agent_at_center=True,
+        penalize_boundary_hit=False
+    ),
+
+    "AGENT": dict(
+        # params controlling the state and state-transitions
+        angle = 45, persistence=0.8, worth_offset=0.05, 
+        noise=0.01, nstep_transition=[1], positive_emphasis=0,
+        skipStep=10, reward_patch_discovery=True, 
+        add_exploration = True, spacings=[],
+
+        # params related to time memory
+        time_memory_factor=0.6, time_memory_exp=1.0,
+        time_memory_sizes= [20,50,100,200,400],
+
+        # other params
+        render=False, 
+        debug=False, debugDir='.temp',
+    ),
+    
+    "ADAM": dict(
+        lr=0.005, weight_decay=0.05
+    ),
+
+    "MULTI_STEP_LR": dict(
+        milestones=[50*i for i in range(1,21)],
+        gamma=0.5
+    ),
+
+    "PER_BUFFER": dict(
+        bufferSize=int(6E4), 
+        alpha=0.95,
+        beta=0.1, 
+        beta_rate=0.9/2000
+    ),
+
+    "TRAINING_STRAT_EPSILON_GREEDY": dict(
+        epsilon=0.5,
+        finalepsilon=0.2,
+        decaySteps=1000
+    ),
+
+    "DDQN": dict(
+        batchSize=512, 
+        gamma=0.9, 
+        update_freq=5, 
+        MaxTrainEpisodes=2000, 
+        optimize_every_kth_action=100, 
+        num_gradient_steps=25,
+        evalFreq=10, 
+        printFreq=1, 
+        polyak_average=True, 
+        polyak_tau=0.1,
+        resumeable_snapshot=10,
+    ),
+
+    "seed": 0, # seed for random, np.random, torch
+    "LOG_DIR_PARENT": ".temp", # the log folder for all runs
+    "RESUME_DIR": None, # set if resuming a run
+    "ENABLE_WANDB": False, # set to true for server env
+}
