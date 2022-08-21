48fd3c273d9d726c46f1c297aa5f0917196b920a
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-08-21 22:39:17 +0530

    work in progress... wandb callbacks

--- DDQN-way/utils/callbacks/wandb_callbacks.py
+++ DDQN-way/utils/callbacks/wandb_callbacks.py
@@ -1,6 +1,7 @@
+import os
 import wandb
 from berry_field import BerryFieldEnv
-
+from utils.visualization import picture_episodes
 
 class wandbMetricsLogger:
     def __init__(self,
@@ -37,3 +38,74 @@ class wandbMetricsLogger:
                 info_dict["eval"][0][key] = val
                 
         wandb.log(info_dict)
+
+
+class wandbEpisodeVideoLogger:
+    def __init__(
+        self, log_dir:str, train_log_freq=100, eval_log_freq=10, figsize=(20,20)
+    ) -> None:
+        """ 
+        NOTE:
+            the working of this function assumes that the info-dict's
+            direct keys may eval if evaluation is enabled, this is used to
+            assert wether or not to try forming the evaluation video.
+            
+            This function assumes that logging is being done by the berry-field-env"""
+        self.log_dir = log_dir
+        self.train_log_freq = train_log_freq
+        self.eval_log_freq = eval_log_freq
+        self.figsize = figsize
+
+        self.train_steps = 1
+        self.eval_steps = 1
+        self.last_train_episode = 0
+        self.last_eval_episode = 0
+
+        pass
+
+    def __call__(self, info_dict:dict):
+        
+        train_video = None
+        eval_video = None
+
+        if "train" in info_dict:
+            if self.train_steps % self.train_log_freq == 0:
+                self.train_steps = 1
+
+                # make continue video/gif and send to wandb
+                current_episode = info_dict["train"]["trainEpisode"]
+                video_fn = f"train-episodes-{self.last_train_episode}-{current_episode}.mp4"
+                video_fp = os.path.join(self.log_dir, video_fn)
+                picture_episodes(
+                    fname=video_fp, LOG_DIR=self.log_dir,
+                    episodes=range(self.last_train_episode, current_episode+1),
+                    figsize=self.figsize, titlefmt='train-episode {}',
+                )
+                train_video= wandb.Video(
+                    data_or_path=video_fp, format='mp4',
+                    fps=1, 
+                    caption= video_fn
+                )
+                self.last_train_episode = current_episode+1
+            else:
+                self.train_steps += 1
+
+        if "eval" in info_dict:
+            # make the eval video if eval is enabled
+            if self.eval_steps % self.eval_log_freq == 0:
+                self.eval_steps = 1
+                current_episode = self.last_eval_episode + self.eval_log_freq - 1
+                eval_log_dir = os.path.join(self.log_dir, 'eval')
+                video_fn = f"eval-episodes-{self.last_eval_episode}-{current_episode}.mp4"
+                video_fp = os.path.join(self.log_dir, video_fn)
+                picture_episodes(
+                    fname=video_fp, LOG_DIR=eval_log_dir,
+                    episodes=range(self.last_eval_episode, current_episode+1),
+                    figsize=self.figsize, titlefmt='eval-episode {}',
+                )
+
+            else:
+                self.eval_steps += 1
+
+
+            
