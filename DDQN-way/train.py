ea16dbafab996b3803e19b899e7cc1647c1acdec
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-08-21 15:11:55 +0530

    integrated wandb logging, added comments and removed unused function

--- DDQN-way/train.py
+++ DDQN-way/train.py
@@ -1,5 +1,5 @@
 import time
-
+import wandb
 from DRLagents import *
 from torch.optim.adam import Adam
 from torch.optim.rmsprop import RMSprop
@@ -14,28 +14,32 @@ torch.manual_seed(0)
 np.random.seed(0)
 random.seed(0)
 
+# high level configs
+WANDB_CALLBACK = False
 LOG_DIR = os.path.join('.temp' , '{}-{}-{} {}-{}-{}'.format(*time.gmtime()[0:6]))
 RESUME_DIR= LOG_DIR
 TORCH_DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# helper functions
 def random_train_env(log_dir):
     print('random_train_env')
     trainEnv = getRandomEnv(field_size=(20000,20000), 
         patch_size=(2600,2600), num_patches=10, seperation=2400, 
-        nberries=80, logDir=log_dir, spawn_radius=100, 
+        nberries=80, spawn_radius=100, 
         patch_with_agent_at_center=True,
-        penalize_boundary_hit=False)
-    return trainEnv
-
-def original_train_env(log_dir):
-    print('original_train_env')
-    trainEnv = BerryFieldEnv(analytics_folder=log_dir, 
-                            penalize_boundary_hit=False)
+        penalize_boundary_hit=False, logDir=log_dir)
     return trainEnv
 
 if __name__ == '__main__':
 
+    # some variables used later
     resume_run = os.path.exists(RESUME_DIR)
+    callbacks = []
+
+    # init wandb callback if enabled
+    if WANDB_CALLBACK:
+        wandb.init(project="test-project", entity="foraging-rl")
+        callbacks.append(wandb.log)
 
     # copy all files into log-dir and setup logging
     logger = StdoutLogger(filename=f'{LOG_DIR}/log.txt')
@@ -51,12 +55,12 @@ if __name__ == '__main__':
     evalEnv.step = agent.env_step_wrapper(evalEnv)
     nnet = agent.getNet(); print(nnet)
 
-    # training stuffs
+    # init training prereqs
     optim = Adam(nnet.parameters(), lr=0.005, weight_decay=0.05)
     schdl = MultiStepLR(optim,[50*i for i in range(1,21)],gamma=0.5)
-    buffer = PrioritizedExperienceRelpayBuffer(int(6E4), alpha=0.95,
+    buffer = PrioritizedExperienceRelpayBuffer(bufferSize=int(6E4), alpha=0.95,
                                         beta=0.1, beta_rate=0.9/2000)
-    tstrat = epsilonGreedyAction(0.5,0.2,decaySteps=1000)
+    tstrat = epsilonGreedyAction(epsilon=0.5,finalepsilon=0.2,decaySteps=1000)
 
     ddqn_trainer = DDQN(trainEnv, nnet, tstrat, optim, buffer, batchSize=512, 
                         gamma=0.9, update_freq=5, MaxTrainEpisodes=2000, lr_scheduler=schdl,
@@ -65,11 +69,12 @@ if __name__ == '__main__':
                         evalFreq=10, printFreq=1, polyak_average=True, polyak_tau=0.1,
                         log_dir=LOG_DIR, resumeable_snapshot=10, device=TORCH_DEVICE)
 
+    # print some of the settings for easier reference
     print(f'optim = {ddqn_trainer.optimizer}, num_gradient_steps= {ddqn_trainer.num_gradient_steps}')
     print(f"optimizing the online-model after every {ddqn_trainer.optimize_every_kth_action} actions")
     print(f"batch size={ddqn_trainer.batchSize}, gamma={ddqn_trainer.gamma}, alpha={buffer.alpha}")
     print(f"polyak_tau={ddqn_trainer.tau}, update_freq={ddqn_trainer.update_freq_episode}")
-    
+
     # try to resume training
     if resume_run: ddqn_trainer.attempt_resume(RESUME_DIR)    
 
@@ -79,7 +84,8 @@ if __name__ == '__main__':
 
     # start training! :D
     try: trianHist = ddqn_trainer.trainAgent(evalEnv=evalEnv,
-            train_printFn=trainPrintFn, eval_printFn=evalPrintFn)
+            train_printFn=trainPrintFn, eval_printFn=evalPrintFn, 
+            training_callbacks=callbacks)
     except KeyboardInterrupt as kb: pass
 
     # final evaluation with render
