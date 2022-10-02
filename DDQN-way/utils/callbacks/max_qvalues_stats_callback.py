d73f55f66b83afbc845076d579696703537894ca
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-10-03 03:14:49 +0530

    minor bugfix

--- DDQN-way/utils/callbacks/max_qvalues_stats_callback.py
+++ DDQN-way/utils/callbacks/max_qvalues_stats_callback.py
@@ -3,5 +3,5 @@ def max_qvalues_stats_callback(info_dict):
     info_dict["train"]["qvalue_stats"]={
         f"action_{action}": qval for action, qval in enumerate(max_qvalues)
     }
-    print("Max q-values:- ", "".join([f"{i}: {x:.3f}" for i,x in enumerate(max_qvalues)]))
+    print("Max q-values:- ", " ".join([f"{i}: {x:.3f}" for i,x in enumerate(max_qvalues)]))
     return info_dict
