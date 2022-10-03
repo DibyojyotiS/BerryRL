81bcdc4c5ed4b8d6adee9a0b8382dd9a377109b7
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-10-03 23:39:00 +0530

    changed hyper params

--- DDQN-way/config.py
+++ DDQN-way/config.py
@@ -17,8 +17,8 @@ CONFIG = {
         noise=0.01, nstep_transition=[1], positive_emphasis=0,
         skipStep=10, patch_discovery_reward=0.5, 
         add_exploration = True, spacings=[],
-        reward_magnification = 1e4/50,
-        perceptable_reward_range = [-float('inf'),float('inf')],
+        reward_magnification = 1e4/25,
+        perceptable_reward_range = [0,2],
 
         # params related to time memory
         time_memory_factor=0.6, time_memory_exp=1.0,
