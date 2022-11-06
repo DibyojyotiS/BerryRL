fe0c919ef3905ccdee45bc39e8a8d159ae195291
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-11-06 12:35:58 +0530

    added bulk update, refactoring of variable names, and sample time-testing code

--- DDQN-way/agent_utils/memories/nearby_berry_memory.py
+++ DDQN-way/agent_utils/memories/nearby_berry_memory.py
@@ -4,62 +4,66 @@ from agent_utils.memories.base_class import MemoryBase
 
 
 @njit
-def updateMinTree(index, berry_score, min_tree):
-    memory_size = len(min_tree)//2
-    idx = index + memory_size
-    min_tree[idx][0] = berry_score
-    min_tree[idx][1] = index
+def njitUpdateMinTree(index, berryScore, minTree):
+    memorySize = len(minTree)//2
+    idx = index + memorySize
+    minTree[idx][0] = berryScore
+    minTree[idx][1] = index
     while idx > 1:
         idx//=2
-        if min_tree[2*idx][0] > min_tree[2*idx+1][0]:
-            min_tree[idx] = min_tree[2*idx+1]
+        if minTree[2*idx][0] > minTree[2*idx+1][0]:
+            minTree[idx] = minTree[2*idx+1]
         else:
-            min_tree[idx] = min_tree[2*idx]
+            minTree[idx] = minTree[2*idx]
 
 
 @njit
-def removeBerriesOutOfRange(agent_pos_xy, max_dist_pop_th, min_dist_pop_th, memory, min_tree):
-    distances = np.sum((memory[:,:-1] - np.asarray(agent_pos_xy))**2, axis=-1)**0.5
-    indices = np.nonzero((distances > max_dist_pop_th) | (distances < min_dist_pop_th))[0]
+def njitRemoveBerriesOutOfRange(agentPosXY, minDistPopTh, maxDistPopTh, memory, minTree):
+    distances = np.sum((memory[:,:-1] - np.asarray(agentPosXY))**2, axis=-1)**0.5
+    indices = np.nonzero((distances < minDistPopTh) | (distances > maxDistPopTh))[0]
     for index in indices:
         memory[index][0] = memory[index][1] = memory[index][2] = -1
-        updateMinTree(index, -1, min_tree)
+        njitUpdateMinTree(index, -1, minTree)
 
 
 @njit
-def njitupdate(berry_score, berry_pos_xy, berry_size, agent_pos_xy, max_dist_pop_th, min_dist_pop_th, memory, min_tree):
+def njitUpdate(berryScore, berryPosXY, berrySize, agentPosXY, minDistPopTh, maxDistPopTh, memory, minTree):
 
     # remove any berry whose distance from agent is is out of range
-    removeBerriesOutOfRange(agent_pos_xy, max_dist_pop_th, min_dist_pop_th, memory, min_tree)
+    njitRemoveBerriesOutOfRange(agentPosXY, minDistPopTh, maxDistPopTh, memory, minTree)
 
     # add the new berry to the memory if it's score is greater than the minimum score
-    dist = ((berry_pos_xy[0] - agent_pos_xy[0])**2 + (berry_pos_xy[1] - agent_pos_xy[1])**2)**0.5
-    if (dist > min_dist_pop_th and dist < max_dist_pop_th):
-        min_berry_score = min_tree[1][0]
-        if berry_score > min_berry_score:
-            min_index = int(min_tree[1][1])
-            memory[min_index] = [berry_pos_xy[0], berry_pos_xy[1], berry_size]
-            updateMinTree(min_index, berry_score, min_tree)
+    dist = ((berryPosXY[0] - agentPosXY[0])**2 + (berryPosXY[1] - agentPosXY[1])**2)**0.5
+    if (dist > minDistPopTh and dist < maxDistPopTh):
+        min_berry_score = minTree[1][0]
+        if berryScore > min_berry_score:
+            min_index = int(minTree[1][1])
+            memory[min_index] = [berryPosXY[0], berryPosXY[1], berrySize]
+            njitUpdateMinTree(min_index, berryScore, minTree)
 
 
 @njit
-def bulkUpdata(listOfBerries, agent_pos_xy, max_dist_pop_th, min_dist_pop_th, memory, min_tree):
+def njitBulkUpdata(listOfBerries, listOfberryScores, agentPosXY, minDistPopTh, maxDistPopTh, memory, minTree):
     for i in prange(len(listOfBerries)):
-        berry_pos_xy = listOfBerries[i, :2]
-        # berry_score = 
-
+        berryScore = listOfberryScores[i]
+        berryPosXY = listOfBerries[i, :2]
+        berrySize = listOfBerries[i, 3]
+        njitUpdate(berryScore, berryPosXY, berrySize, agentPosXY, minDistPopTh, maxDistPopTh, memory, minTree)
 
 
 class NearbyBerryMemory(MemoryBase):
-    def __init__(self, dist_th1, dist_th2, memory_size) -> None:
-        self.min_dist_pop_th = dist_th1
-        self.max_dist_pop_th = dist_th2
-        self.memory_size = memory_size
+    def __init__(self, minDistPopTh, maxDistPopTh, memorySize) -> None:
+        self.minDistPopTh = minDistPopTh
+        self.maxDistPopTh = maxDistPopTh
+        self.memorySize = memorySize
 
         self.__initMemory()
 
-    def update(self, berry_score, berry_pos_xy, berry_size, agent_pos_xy):
-        njitupdate(berry_score, berry_pos_xy, berry_size, agent_pos_xy, self.max_dist_pop_th, self.min_dist_pop_th, self.memory, self.min_tree)
+    def update(self, berryScore, berryPosXY, berrySize, agentPosXY):
+        njitUpdate(berryScore, berryPosXY, berrySize, agentPosXY, self.minDistPopTh, self.maxDistPopTh, self.memory, self.minTree)
+
+    def bulkUpdate(self, listOfBerries, listOfberryScores, agentPosXY):
+        njitBulkUpdata(listOfBerries, listOfberryScores, agentPosXY, self.minDistPopTh, self.maxDistPopTh, self.memory, self.minTree)
 
     def getMemoryBerries(self):
         return NearbyBerryMemory.__filterMemory(self.memory)
@@ -68,21 +72,58 @@ class NearbyBerryMemory(MemoryBase):
         self.__initMemory()
 
     def __initMemory(self):
-        self.memory = np.zeros((self.memory_size, 3), dtype=np.float32) # x, y, berry_size
-        self.min_tree = np.zeros((2*self.memory_size, 2)) # berry_score, memory-index
+        self.memory = np.zeros((self.memorySize, 3), dtype=np.float32) # x, y, berry_size
+        self.minTree = np.zeros((2*self.memorySize, 2)) # berry_score, memory-index
         self.size = 0
         self.idx = 0
 
         # init min-tree
-        for i in range(self.memory_size):
-            updateMinTree(i, -1, self.min_tree)
+        for i in range(self.memorySize):
+            njitUpdateMinTree(i, -1, self.minTree)
 
     @staticmethod
     @njit
     def __filterMemory(memory = np.array([[]])):
         indices = np.nonzero(memory[:,-1] > 0)[0]
-        return_array = np.zeros((len(indices), 3))
+        returnArray = np.zeros((len(indices), 3))
         for i, index in enumerate(indices):
             for j in range(3):
-                return_array[i][j] = memory[index][j]
-        return return_array
+                returnArray[i][j] = memory[index][j]
+        return returnArray
+
+
+if __name__ == "__main__":
+    from time import time_ns
+
+    def temp_berry_worth(sizes:np.ndarray, distances:np.ndarray, 
+                REWARD_RATE, DRAIN_RATE, HALFDIAGOBS, WORTH_OFFSET=0,
+                min_berry_size=10, max_berry_size=40):
+        rr, dr = REWARD_RATE, DRAIN_RATE
+        worth = rr * sizes - dr * distances
+        min_worth = rr * min_berry_size - dr * HALFDIAGOBS
+        max_worth = rr * max_berry_size
+        worth = (worth - min_worth)/(max_worth - min_worth)
+        worth = np.clip(worth, a_min=0,a_max=None)
+        worth = (worth + WORTH_OFFSET)/(1 + WORTH_OFFSET)
+        return worth
+
+    def setup():
+        xy = np.random.randint(-1300, 1300, size=(80,2))
+        sizes = np.random.randint(1,5, size=80)*10
+        berries = np.column_stack([xy[:,0], xy[:,1], sizes])
+        distances = np.linalg.norm(xy, axis=1)
+        worths = temp_berry_worth(sizes, distances, REWARD_RATE=1e-4, DRAIN_RATE=1/(2*120*400), HALFDIAGOBS = 0.5*(1920**2 + 1080**2)**0.5)
+        return berries, worths
+
+    mem = NearbyBerryMemory(10, 150, 10)
+    berries, worths = setup()
+    mem.bulkUpdate(berries, worths, (0,0))
+    mem.reset()
+
+    times = []
+    for i in range(10000):
+        berries, worths = setup()
+        startT = time_ns()
+        mem.bulkUpdate(berries, worths, (0,0))
+        times.append(time_ns() - startT)
+    print("average time bulkUpdate:", np.average(times), "ns")
