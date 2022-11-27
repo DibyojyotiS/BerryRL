a18e0f2a7f92ff8469c9622aabc25d3f98934332
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-11-28 00:36:49 +0530

    locality memory added as 'to-use'

---
+++ agent-trainer/agent/memories/locality_memory.py
@@ -0,0 +1,33 @@
+import numpy as np
+from typing import Tuple
+from .base_class import MemoryBase
+
+class LocalityMemory(MemoryBase):
+    def __init__(
+        self,
+        berryfield_FIELD_SIZE: Tuple[int, int],
+        resolution: Tuple[int, int]
+    ) -> None:
+        self.field_size = berryfield_FIELD_SIZE
+        self.resolution = resolution
+        self._initialize_localities()
+        
+    def _initialize_localities(self):
+        self.size_x = self.field_size[0]//self.resolution[0]
+        self.size_y = self.field_size[1]//self.resolution[1]
+        self.visited_localities = np.zeros(self.resolution)
+
+    def reset(self, *args, **kwargs):
+        self.visited_localities[:] = 0
+
+    def update(self, agent_pos_xy:Tuple[int, int]):
+        x,y = agent_pos_xy
+        if x == self.field_size[0]: x -= 1e-6
+        if y == self.field_size[1]: y -= 1e-6
+        
+        locality_x = int(x/self.size_x)
+        locality_y = int(y/self.size_y)
+        self.visited_localities[locality_x][locality_y] = 1
+
+    def get(self):
+        return self.visited_localities
