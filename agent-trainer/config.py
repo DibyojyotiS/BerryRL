ef5f6823b69ed141b8ca502bb017007078ab925c
DibyojyotiS <dibyojyoti21century@gmail.com>
2022-11-29 00:32:42 +0530

    initial config

--- agent-trainer/config.py
+++ agent-trainer/config.py
@@ -1,34 +1,22 @@
 CONFIG = {
     "seed": 4,
-    "LOG_DIR_ROOT": ".temp/single-experiments/reward-shaping",
+    "LOG_DIR_ROOT": ".temp/single-experiments/tackle-loss/v1",
     "WANDB": dict(
         enabled = True, # set to true for server env
         project="agent-design-v1",
-        group="single-experiments",
+        group="single-experiments/tackle-loss",
         entity="foraging-rl",
         watch_log_freq = 100,
-        notes=(
-            "the berries from memory and current observation are taken " 
-            + "together for the computation of the sectorized state, "
-            + "the maxObsDiag parameter is adjusted to the maximum limit of memory "
-            + "this allows enough wiggle of values in the sectorized state. "
-            + "random exploration action is enabled. "
-            + "And also allow the agent to play till max-time (5 in game minutes). "
-            + """The reward perception is that the more berries the agent 
-            collects the larger the reward on the next berry collected (the
-            positive part of reward is nBerriesPicked/100 + scale*actual_reward). 
-            The rest of the negative rewards are same as the env reward. """
-            + "Optimizing the model at the episode end. "
-            + """
-            steps to curb increasin loss:
-                decreased priority alpha
-                clipping gradient by norm instead of value
-                increased update freq to 40
-                >> WARN: reduced batch-size to 512 to increaes comp. speed
-                >> however this shouldn't affect the convergence behaviour of
-                >> loss appart from creating noise.
-            """
-            )
+        notes="""
+        - The berries from memory and current observation are taken together 
+            for the computation of the sectorized state. 
+        - Random exploration action is enabled.
+        - Agent is to play till max-time (5 in game minutes).
+        - Reward Perception:
+             actual_reward > 0: reward = nBerries/100 + scale*actual_reward
+             actual_reward < 0: reward = actual_reward
+        - Optimizing the model at the episode end.
+        """
     ),
     "RND_TRAIN_ENV": dict(
         field_size=(20000,20000), 
@@ -56,26 +44,29 @@ CONFIG = {
                 minDistPopThXY=(1920/2, 1080/2), 
                 maxDistPopThXY=(2600,2600), 
                 memorySize=50
+            ),
+            localityMemoryKwars = dict(
+                resolution = (5,5)
             )
         ),
         state_computation_config = dict(
-            persistance=0.8, 
+            persistance=0.0, 
             sector_angle=45,
-            berryworth_offset=0.05,
-            max_berry_count = 800,
-            noise=0.05
+            berryworth_offset=0.01,
+            max_berry_count = 200,
+            noise=0.005
         ),
         exploration_subroutine_config = dict(
-            reward_discount_factor=1.0,
+            reward_discount_factor=0.99,
             max_steps=float('inf')
         ),
         reward_perception_config = dict(
             max_clip=float('inf'), min_clip=-float('inf'),
-            scale=3 * 1e4/50
+            scale=200
         ),
         nn_model_config = dict(
             layers=[32,16,16],
-            lrelu_negative_slope=-0.01
+            lrelu_negative_slope=-0.0
         )
     ),
     
@@ -84,7 +75,7 @@ CONFIG = {
     ),
 
     "MULTI_STEP_LR": dict(
-        milestones=[100*i for i in range(1,3)],
+        milestones=[200*i for i in range(1,3)],
         gamma=0.5
     ),
 
@@ -98,23 +89,23 @@ CONFIG = {
     "TRAINING_STRAT_EPSILON_GREEDY": dict(
         epsilon=0.55,
         finalepsilon=0.1,
-        decaySteps=200,
+        decaySteps=500,
         decay_type='exp'
     ),
 
     "DDQN": dict(
-        batchSize=512, 
-        gamma=0.99, 
+        batchSize=256, 
+        gamma=0.9, 
         update_freq=40, 
         MaxTrainEpisodes=2000, 
         MaxStepsPerEpisode=None,
-        optimize_every_kth_action=-1, #-1, 
-        num_gradient_steps=250, #400,
+        optimize_every_kth_action=-1,
+        num_gradient_steps=400,
         evalFreq=10, 
         printFreq=1, 
         polyak_average=True, 
-        polyak_tau=0.05,
-        gradient_clip = 1,
+        polyak_tau=0.1,
+        gradient_clip = 5,
         gradient_clipping_type = "norm",
     ),
 }
