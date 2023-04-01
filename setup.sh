5c0e9c12ca63f3c66cde193c8659876f3135f4bf
DibyojyotiS <dibyojyoti21century@gmail.com>
2023-04-02 01:18:10 +0530

    fixes

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if conda info --envs | grep -q ML; then echo "base already exists"; else conda create -y -n ML; fi
+if conda info --envs | grep -q ML; then echo "base already exists"; else conda create -n ML python=3.7.6; fi
 conda init
 
 conda activate ML
 conda env list
-if conda info --envs | grep -q ML; then echo "Created env ML"; else echo "failed somewhere"; exit; fi
+if conda env list | grep "ML * [\*]"; then echo "Created env ML"; else echo "failed somewhere"; exit; fi
 
 pip install -r requirements.txt
 pip install -e berry-field
